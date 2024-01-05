eebbc54ebe073b1d7a8141811817659ca61e9eb1
Ethan Husband <ethan.husband@evup.com.au>
2024-01-06 10:44:17 +1100

    LM-86: Update tips

--- src/widgets/account/dropdowns/TipsDropdown.tsx
+++ src/widgets/account/dropdowns/TipsDropdown.tsx
@@ -4,11 +4,11 @@ import { SettingsDropdowns } from "./constants";
 import { BulletedText } from "../../../components/Text";
 
 const TIPS = [
-  "To automatically include tasks/events in each week, add them to your Routine",
-  "To delete any task or event, hold down on the item",
+  "To modify details of any task or event, swipe it left!",
+  "To automatically include items in each week, add them to your Routine",
+  "To delete any task or event, hold it down",
   "To hide the contents of the week, press the button where it's dates are shown",
   "To hide a day, hold down it's green header section",
-  "With Premium, swipe an event or task left to plan in greater detail",
 ];
 
 export const TipsDropdown = ({ settingOpen, setOpen }) => {
@@ -17,7 +17,6 @@ export const TipsDropdown = ({ settingOpen, setOpen }) => {
       name="Tips"
       touchableHightlightExtraStyles={{ paddingLeft: 2 }}
       boldTitle
-      isBottom
       open={settingOpen === SettingsDropdowns.Tips}
       onPress={() => setOpen(SettingsDropdowns.Tips)}
     >
