6251014008fc1e51d37ec48a3c392a60bba28b9a
Ethan Husband <ethan.husband@evup.com.au>
2024-01-13 16:12:01 +1100

    VERSION 1.4.0

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default {
     scheme: "lyf",
     currentFullName: "@ethanhusband/lyf",
     originalFullName: "@ethanhusband/lyf",
-    version: "1.3.1",
+    version: "1.4.0",
     icon: "./assets/images/inverted-icon.png",
     orientation: "portrait",
     userInterfaceStyle: "light",
@@ -43,11 +43,11 @@ export default {
         backgroundColor: "#ffffff",
       },
       package: "com.lyfplanner",
-      versionCode: 131,
+      versionCode: 140,
     },
     ios: {
       bundleIdentifier: "com.lyfplanner",
-      buildNumber: "1.3.1",
+      buildNumber: "1.4.0",
     },
     plugins: [],
     extra: {
