1bd5a18070c9bac84841562b092354fbe987ebad
Ethan Husband <ethan.husband@evup.com.au>
2024-03-03 16:45:20 +1100

    LM-333: Revert envManager to get both manifests

--- src/envManager.ts
+++ src/envManager.ts
@@ -1,8 +1,22 @@
 import Constants from "expo-constants";
-const { manifest2 } = Constants;
+const { manifest, manifest2 } = Constants;
 
+// With expo constants, once Constants.manifest is null, Constants.manifest2 contains the env vars
+// Which of these contains data depends on environments
+// This unifies them into one function call so we don't have to worry about distinguishing elsewhere
 function envVar(varName: string, subvar_name: string = null) {
-  return manifest2?.extra?.expoClient?.extra?.[varName];
+  // Slower but more robust method of finding env var
+  // @ts-ignore
+  for (const extra of [manifest?.extra, manifest2?.extra]) {
+    let c;
+    if (subvar_name)
+      c =
+        extra?.[varName][subvar_name] ??
+        extra?.expoClient?.extra?.[varName][subvar_name];
+    else c = extra?.[varName] ?? extra?.expoClient?.extra?.[varName];
+    if (c) return c;
+  }
+  throw new Error(`Constant ${varName} not found in expo manifest.`);
 }
 
 function parseBackendUrl() {
@@ -26,7 +40,10 @@ function parseBackendUrl() {
 
   // The IP address of the machine hosting the expo app can be found in manifest2.launchAsset or manifest.debuggerHost
   // Which one is present depends on which of manifest or manifest2 is null - which varies across environments
-  var debuggerUrl = manifest2.launchAsset.url;
+  var debuggerUrl = manifest
+    // @ts-ignore
+    ? manifest.debuggerHost
+    : manifest2.launchAsset.url;
 
   // Retrieve IP from URL
   var ip = extractIPfromURL(debuggerUrl);
