2bca3d832064c9550252393da8cb198c780e62af
Ethan Husband <eth.husband@gmail.com>
2024-12-28 18:55:36 +1100

    VERSION 2.1.1

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default {
     scheme: 'lyf',
     currentFullName: '@ethanhusband/lyf',
     originalFullName: '@ethanhusband/lyf',
-    version: '2.1.0',
+    version: '2.1.1',
     icon: './src/assets/images/inverted-icon.png',
     orientation: 'portrait',
     newArchEnabled: true,
@@ -47,11 +47,11 @@ export default {
         backgroundColor: '#ffffff'
       },
       package: 'com.lyfplanner',
-      versionCode: 2100
+      versionCode: 2101
     },
     ios: {
       bundleIdentifier: 'com.lyfplanner',
-      buildNumber: '2.1.0'
+      buildNumber: '2.1.1'
     },
     plugins: [
       'expo-asset',
